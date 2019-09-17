44c94411cc43e907823c996cce46db34a82ac0ba
Lawrence Wang <setofvs@gmail.com>
2019-09-17 11:18:00 -0400

    Updated User DB file

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,10 +3,20 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING
+    // allowNull: false
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    // allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  address: {
+    type: Sequelize.TEXT
   },
   password: {
     type: Sequelize.STRING,
