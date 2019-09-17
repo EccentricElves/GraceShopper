757ff7933332fc3939eaf401da98797b06d30292
Brian Oblad <a.gray.cloud@gmail.com>
2019-09-17 18:24:19 -0400

    trying to figure how to make through table work

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,4 +1,32 @@
 const router = require('express').Router()
-const {Order, User, Art} = require('../db')
+const {Order, Art} = require('../db/models')
+
+router.put('/:artId', async (req, res, next) => {
+  try {
+    const newOrder = await Order.findOrCreate({
+      where: {
+        userId: /*req.user.id*/ 1,
+        status: 'pending'
+      }
+    })
+    console.dir(newOrder)
+    // set the art product of the item
+    const artItem = await Art.findOne({
+      where: {
+        id: req.params.artId
+      }
+    })
+    console.dir(artItem)
+
+    //newOrder.addDetail(artItem)
+    artItem.addOrder(newOrder)
+
+    //user ---> order <---> item
+
+    res.json(newOrder)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
