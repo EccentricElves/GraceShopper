d7edfce8603c71d4e7e8f9e713d12a29de4b70f2
Rushil Shakya <rushil.shakya@gmail.com>
2019-09-18 17:52:42 -0400

    Fixed #31 for adding to cart for guest

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -15,6 +15,7 @@ const Navbar = ({handleClick, isLoggedIn, email, name}) => {
           <div>
             {/* The navbar will show these links after you log in */}
             <Link to="/">Home</Link>
+            <Link to="/cart">Cart</Link>
             <a href="#" onClick={handleClick}>
               Logout
             </a>
@@ -24,6 +25,7 @@ const Navbar = ({handleClick, isLoggedIn, email, name}) => {
           <div>
             {/* The navbar will show these links before you log in */}
             <Link to="/">Home</Link>
+            <Link to="/cart">Cart</Link>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
             <Switch>
--- client/store/cart.js
+++ client/store/cart.js
@@ -9,7 +9,16 @@ const addedArt = art => ({
 
 export const addArt = artId => {
   return async dispatch => {
-    const {data} = await axios.put(`/api/order/add/${artId}`)
-    dispatch(addedArt(data))
+    try {
+      const {data} = await axios.put(`/api/order/add/${artId}`)
+      dispatch(addedArt(data))
+    } catch (error) {
+      let myData = JSON.parse(localStorage.getItem('cart')) || {data: []}
+      if (!myData.data.map(x => x.id).includes(artId)) {
+        const {data} = await axios.get(`/api/art/${artId}`)
+        myData = {...myData, data: [...myData.data, data]}
+        localStorage.setItem('cart', JSON.stringify(myData))
+      }
+    }
   }
 }
