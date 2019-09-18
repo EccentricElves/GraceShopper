99d9b88cb0c5da2e4f7cf41e9a4fb99012282360
Brian Oblad <a.gray.cloud@gmail.com>
2019-09-17 22:16:28 -0400

    cart route for review

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,29 +1,46 @@
 const router = require('express').Router()
 const {Order, Art} = require('../db/models')
 
+router.get('/cart/:userId', async (req, res, next) => {
+  try {
+    const order = await Order.findOne({
+      where: {
+        userId: req.params.userId
+      },
+      include: [{model: Art}]
+    })
+
+    res.json(order)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.put('/:artId', async (req, res, next) => {
   try {
-    const newOrder = await Order.findOrCreate({
+    const [order] = await Order.findOrCreate({
       where: {
-        userId: /*req.user.id*/ 1,
+        userId: req.user.id,
         status: 'pending'
       }
     })
-    console.dir(newOrder)
-    // set the art product of the item
+
     const artItem = await Art.findOne({
       where: {
         id: req.params.artId
       }
     })
-    console.dir(artItem)
 
-    //newOrder.addDetail(artItem)
-    artItem.addOrder(newOrder)
+    // the concept of an Order Model having a quantity column to keep track of amt
+    // in this case we would establish the association Art.hasMany(Order)
+    // so our query for this route could search artId: req.params.artId
+    // order.update({quantity: order.quantity + 1})
+
+    order.addArt(artItem)
 
     //user ---> order <---> item
 
-    res.json(newOrder)
+    res.json(order)
   } catch (error) {
     next(error)
   }
