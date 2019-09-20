33dc2f25b939c3e9e115956bf23abd24920ffbae
Brian Oblad <a.gray.cloud@gmail.com>
2019-09-20 15:39:29 -0400

    toast

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
+import ReduxToastr from 'react-redux-toastr'
 import history from './history'
 import store from './store'
 import App from './app'
@@ -12,6 +13,15 @@ import './socket'
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
+      <ReduxToastr
+        timeOut={4000}
+        newestOnTop={false}
+        preventDuplicatates
+        position="top-left"
+        transitionIn="fadeIn"
+        transitionOut="fadeOut"
+        closeOnToastrClick
+      />
       <App />
     </Router>
   </Provider>,
