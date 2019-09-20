d7a0e11bb70ee05b8b8e22de946127082d058ea8
Brian Oblad <a.gray.cloud@gmail.com>
2019-09-20 15:46:56 -0400

    remove logs

--- client/components/cartButton.js
+++ client/components/cartButton.js
@@ -13,7 +13,6 @@ class DisconnectedCartButton extends Component {
   handleOnClick() {
     this.props.addToCart(this.props.productId)
     toastr.success('Nice!', 'You have added an Item to your Cart')
-    console.log('running after dispatch')
   }
 
   render() {
